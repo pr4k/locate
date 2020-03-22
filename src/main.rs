32c4ac89c00a5e6a85a293b6040e202c728fc7f1
Prakhar <b118038@iiit-bh.ac.in>
2020-03-22 11:03:43 +0000

    Modified: main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ use walkdir::WalkDir;
 
 fn check_dir(path: &str, query: &str) {
     let mut total_files_scanned = 0;
-    for (fl_no, e) in WalkDir::new(path)
+    for (fl_no, file) in WalkDir::new(path)
         .into_iter()
-        .filter_map(|e| e.ok())
+        .filter_map(|file| file.ok())
         .enumerate()
     {
-        if e.metadata().unwrap().is_file() {
-            match fstream::contains(e.path(), query) {
+        if file.metadata().unwrap().is_file() {
+            match fstream::contains(file.path(), query) {
                 Some(b) => {
                     if b {
-                        check_file(e.path(), query);
+                        check_file(file.path(), query);
                     }
                 }
                 None => println!("Error in walking Dir"),
@@ -40,15 +40,14 @@ fn check_file(file_path: &Path, query: &str) {
         file_path.display().to_string().magenta().italic()
     );
     match fstream::read_lines(file_path) {
-        Some(s) => {
-            for (pos, s) in &mut s.iter().enumerate() {
-                if s.contains(query) {
-                    let s = s.trim().to_string();
-                    let mut s = s.clone();
-                    s.truncate(2000);
+        Some(lines) => {
+            for (pos, line) in &mut lines.iter().enumerate() {
+                if line.contains(query) {
+                    let mut line = line.trim().to_string().clone();
+                    line.truncate(2000);
                     print!("{}", "Line ".green().bold());
                     print!("{0: <6} ", pos.to_string().cyan());
-                    println!("=> {}", s.blue());
+                    println!("=> {}", line.blue());
                 }
             }
         }
